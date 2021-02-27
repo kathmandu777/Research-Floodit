051f3c634d4dd7989a6256a72b009046f928ecf4
kathmandu <mahhakatomanato@gmail.com>
2021-02-27 11:21:53 +0900

    floodit_testの修正

--- gym-floodit/floodit_test.py
+++ gym-floodit/floodit_test.py
@@ -9,7 +9,7 @@ observation = env.reset()
 for _ in range(1000):
     env.render()
     action = env.action_space.sample()  # random action
-    observation, reward, done, _ = env.step(action)
+    observation, reward, done, info = env.step(action)
 
     if done:
         env.reset()
